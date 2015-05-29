fe3691242e2b9a2bfe583362330f11439f4d7133
Pannuba <carlomaria.99@hotmail.it>
2015-05-29 15:24:00 +0200

    mem

--- Dev-Cpp/Statistica/main.cpp
+++ Dev-Cpp/Statistica/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
-#include <string>
 using namespace std;
+
 int main(){
 	
 	system("color a");
-	unsigned long long int i = 0, j = 0;
-    float tot, valori[i], somma, media, hold;         // Se tolgo i = 0 va in crisi
+	int tot, i = 0, j = 0, med1, med2;
+    float valori[i], somma, media, hold, mediana, cvar, scr, scarto, dev, deviazione;      // Se tolgo i = 0 va in crisi
     
     cout << "PannCoseSullaStatistica";
     
@@ -15,6 +15,7 @@ int main(){
 	
 	{
         somma = 0;
+        mediana = 0;
         cout << "\n\nNumero totale elementi? ";
 		cin >> tot;
 		
@@ -76,14 +77,54 @@ int main(){
 	
         }
 	
-        cout << "\n\nIn ordine dal piu basso al piu alto sono:\n\n";
+        /*cout << "\n\nIn ordine dal piu basso al piu alto sono:\n\n";
 
         for (i = 0; i < tot; i++)
 	
-        {
+        { 												//  QUESTO NON SERVE!!!!!!!!1 
 	        cout << valori[i] << endl;
-        }   
-            
+        }*/
+        //tot2 = tot / 2;
+        
+        if (tot / 2 != static_cast <int> (tot / 2))			// se i valori totali sono dispari
+        
+		{
+			med1 = (tot / 2 + 0.5) - 1;								//non va
+		    mediana = valori[med1];
+		    cout << "\n\nMediana: " << mediana;
+		}
+		
+		else												//se pari
+		
+		{
+		    med1 = (tot / 2 + 1) - 1;
+		    med2 = (tot / 2) - 1;
+		    mediana = (valori[med1] + valori[med2]) / 2;
+		    cout << "\n\nMediana: " << mediana;
+		}
+		
+        cvar = valori[tot - 1] - valori[0];
+        cout << "\n\nCampo di variazione: " << cvar;
+        scr = 0;
+        
+        for (i = 0; i < tot; i++)
+        
+        {
+            scr = scr + (valori[i] - media);
+        }
+        
+        scarto = scr / tot;
+        cout << "\n\nScarto semplice medio: " << scarto;
+        dev = 0;
+        
+        for (i = 0; i < tot; i++)
+        
+        {
+            dev = dev + powf(valori[i] - media, 2);
+        }
+        
+        deviazione = sqrt(dev / tot);
+        cout << "\n\nDeviazione standard: " << deviazione;
 	}
 	
 	cout << "\n\nPremere un qualsiasi tasto per uscire: ";
