364f554497f520374357a568b94fe229daaa1729
Pannuba <carlomaria.99@hotmail.it>
2015-07-28 19:43:11 +0100

    Create music.h

---
+++ C++/Musica/akk.li/music.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+void music()
+
+{
+	Beep(783.99, 500);
+	Beep(739.99, 500);
+	Beep(783.99, 500);
+	Beep(783.99, 250);
+	Beep(880.00, 250);
+	Beep(783.99, 500);
+	Beep(739.99, 500);
+	Beep(659.25, 500);
+	Beep(493.88, 250);
+	Beep(880.00, 250);
+	Beep(783.99, 500);
+	Beep(739.99, 500);
+	Beep(783.99, 500);
+	Beep(739.99, 250);
+	Beep(783.99, 250);
+	Beep(739.99, 500);
+	Beep(659.25, 500);
+	Beep(739.99, 500);
+	Beep(659.25, 250);
+	Beep(880.00, 250);
+}
