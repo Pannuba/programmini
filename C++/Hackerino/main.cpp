10aaefffbcf6656edf6fd41fc1ee1c905b3d8c57
Pannuba <carlomaria.99@hotmail.it>
2015-08-26 01:58:19 +0200

    Update main.cpp

--- C++/Hackerino/main.cpp
+++ C++/Hackerino/main.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <cstdlib>
-#include <string>
 #include <ctime>
 using namespace std;
 
 int main(){
 	
 	system("color a");
+        srand((unsigned) time(0));
 	
 	for ( ;; )
 	
 	{
-	    char carr[60] = {'a','b','c','d','e','f','g','h','i','j','k','l','m',
+	    char carr[59] = {'a','b','c','d','e','f','g','h','i','j','k','l','m',
 						'n','o','p','q','r','s','t','u','v','w','x','y','z',
 						'!','%','^','&','*','(',')','-','=','+','_','{',
 						'}','~','@',':','?','>','<',',','.','/',';','1','2',
 						'3','4','5','6','7','8','9','0'};
-		cout <<	carr[rand() % 60];
+		cout <<	carr[rand() % 59];
 		
 		if ((rand() % 2) == 1)
 		
