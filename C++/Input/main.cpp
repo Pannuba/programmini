20460b922356de2aa88a0b3d7eeefc715df660e2
Pannuba <carlomaria.99@hotmail.it>
2015-08-26 12:17:25 +0200

    Update main.cpp

--- C++/Input/main.cpp
+++ C++/Input/main.cpp
@@ -1,20 +1,47 @@
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
-#include <conio.h>
+#include <string>
 using namespace std;
 
 int main(){
 	
 	system ("color a");
-	cout << "PannTecstEditor\n\n";
+	cout << "PannTecstEditor - scrivi "HOSMESSODISCRIVEREH" per terminare l'input\n\n";
 	string testo;
     ofstream file;
     file.open("zeDispenser.txt");
-    getline (cin, testo); // Il file che crea si chiama zeDispenser.txt // Aggiungere percorso?
+    getline (cin, testo); // Il file che crea si chiama zeDispenser.txt Aggiungere percorso?
 	file << testo; // Scrive nel file questa robba
+    /*
+    string parag[]; unsigned int dimensione;
+    for (int i = 0; i < dimensione; i++)
+    {
+        getline(cin, parag[i]);
+        
+        if (parag[i] == "HOSMESSODISCRIVEREH") // "" o " "??
+
+        {
+            goto fine;
+        }
+
+        system("cls"); // sorry
+        cout << parag[i] << "\n\n";
+        dimensione++;
+        tot += i;
+    }
+
+    fine:
+
+    for (int i = 0; i < parag[tot]; i++)
+    {
+     file << parag[i] << "\n\n"
+    }
+*/
+
     cout << "\n\nPremi Invio per uscire: ";
     file.close();
     cin.ignore();
+    cin.get();
 	return EXIT_SUCCESS;
 }
